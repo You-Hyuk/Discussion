826d6c317170e0787b74d651bcb2c63a21d03e8b
You-Hyuk <cxz6411@naver.com>
2024-10-27 22:10:15 +0900

    fix: Writer/Reader to Stream

--- src/server/repository/ChatRepository.java
+++ src/server/repository/ChatRepository.java
@@ -3,29 +3,33 @@ package server.repository;
 import server.domain.Chat;
 import server.domain.Room;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class ChatRepository {
-    FileWriter fw = null;
+
+    private FileInputStream fis;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+    private FileOutputStream fos;
 
     public void createChatFile(Room room){
         String roomName = room.getRoomName();
-        File file = new File(roomName + ".txt");
+        File file = new File("./data/" + roomName + ".txt");
     }
 
     public void saveChat(Room room, Chat chat){
         try {
             String filePath = room.getRoomName() + ".txt";
-            fw = new FileWriter(filePath); //InputThread에서 BufferedReader로 읽은 line을 저장하기 위해 Writer 사용
+            fos = new FileOutputStream(filePath); //InputThread에서 BufferedReader로 읽은 line을 저장하기 위해 Writer 사용
+            oos = new ObjectOutputStream(fos);
             String chatHistory = chat.toString();
-            fw.write(chatHistory);
+            oos.writeObject(chatHistory);
         }catch (IOException e){
             e.getMessage();
         }finally {
             try {
-                fw.close();
+                oos.close();
+                fos.close();
             }catch (IOException e){
                 e.printStackTrace();
             }
