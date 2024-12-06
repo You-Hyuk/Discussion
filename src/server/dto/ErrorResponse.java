b3ee6e5e92897b8adf373a46bf21855e5d6bec8a
You-Hyuk <cxz6411@naver.com>
2024-12-06 19:50:33 +0900

    feat: 서버 Response Enum 클래스 생성

---
+++ src/server/dto/ErrorResponse.java
@@ -0,0 +1,5 @@
+package server.dto;
+
+public enum ErrorResponse {
+    FIND_ROOM_FAILED;
+}
---
+++ src/server/dto/SuccessResponse.java
@@ -0,0 +1,7 @@
+package server.dto;
+
+public enum SuccessResponse {
+    GET_ROOM_LIST_SUCCESS,
+    FIND_ROOM_SUCCESS,
+    ENTER_ROOM_SUCCESS;
+}
