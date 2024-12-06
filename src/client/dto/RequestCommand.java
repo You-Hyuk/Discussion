bcb409b23bffe287217de56c27f99f86ef76d144
You-Hyuk <cxz6411@naver.com>
2024-12-06 19:50:46 +0900

    feat: Chat Command 추가

--- src/client/dto/RequestCommand.java
+++ src/client/dto/RequestCommand.java
@@ -1,5 +1,11 @@
 package client.dto;
 
 public enum RequestCommand {
-    GET_ROOM_LIST, FIND_ROOM, CREATE_ROOM, ENTER_ROOM;
+    // Room Command
+    GET_ROOM_LIST, FIND_ROOM, CREATE_ROOM, ENTER_ROOM,
+
+    // Chat Command
+    GET_CHAT_HISTORY, SEND_CHAT
+
+    ;
 }
