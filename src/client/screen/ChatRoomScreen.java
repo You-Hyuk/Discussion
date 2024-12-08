50984a919d7a6fd5cc564bf70059b5d883c3dc43
WIN-EHIECQ01T4M\gangh <Ganghyeon5297@naver,com>
2024-12-08 17:44:41 +0900

    스크롤바추가

--- src/client/screen/ChatRoomScreen.java
+++ src/client/screen/ChatRoomScreen.java
@@ -117,6 +117,8 @@ public class ChatRoomScreen {
         status1ChatArea.setLayout(new BoxLayout(status1ChatArea, BoxLayout.Y_AXIS)); // 세로로 정렬
         status1ChatArea.setBackground(Color.WHITE);
         JScrollPane status1ScrollPane = new JScrollPane(status1ChatArea);
+        status1ScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        status1ScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         status1Panel.add(status1Header, BorderLayout.NORTH); // 헤더 패널 추가
         status1Panel.add(status1ScrollPane, BorderLayout.CENTER); // 스크롤 패널 추가
@@ -135,6 +137,8 @@ public class ChatRoomScreen {
         status2ChatArea.setLayout(new BoxLayout(status2ChatArea, BoxLayout.Y_AXIS)); // 세로로 정렬
         status2ChatArea.setBackground(Color.WHITE);
         JScrollPane status2ScrollPane = new JScrollPane(status2ChatArea);
+        status2ScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        status2ScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         status2Panel.add(status2Header, BorderLayout.NORTH); // 헤더 패널 추가
         status2Panel.add(status2ScrollPane, BorderLayout.CENTER); // 스크롤 패널 추가
@@ -316,7 +320,7 @@ public class ChatRoomScreen {
         messagePanel.add(Box.createHorizontalStrut(10));
         messagePanel.add(likeButton);
         messagePanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 120));
-        messagePanel.setPreferredSize(new Dimension(0, 50));
+        messagePanel.setPreferredSize(new Dimension(0, 80));
 
         JPanel emptyPanel = new JPanel();
         emptyPanel.setLayout(new BorderLayout());
@@ -324,7 +328,7 @@ public class ChatRoomScreen {
         emptyPanel.add(new JLabel(" "), BorderLayout.CENTER);
         emptyPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, 120)); // 최대 크기 설정
         emptyPanel.setMinimumSize(new Dimension(0, 50)); // 최소 크기 설정
-        emptyPanel.setPreferredSize(new Dimension(0, 50));
+        emptyPanel.setPreferredSize(new Dimension(0, 80));
 
         int linesToSync;
         if (status.equals(firstStatus)) {
